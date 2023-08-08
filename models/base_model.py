25d298910076e50b8671e3785c511b6754b7c8ed
syphrose <swabulwenyi@gmail.com>
2023-08-08 03:31:33 -0700

    Basemodel from the dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,7 +8,13 @@ other classes
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                elif key != "__class__":
+                    setattr(self, key, value)
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
