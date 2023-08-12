63357c0e08d0601924472f1cb416bc53479bb0e3
TechDanny <kokidanny15@gmail.com>
2023-08-12 02:57:14 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -12,15 +12,18 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         if len(kwargs) != 0:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
+                if key == '__class__':
+                    continue
+                elif key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.strptime(value,
                             "%Y-%m-%dT%H:%M:%S.%f"))
-                elif key != "__class__":
+                else:
                     setattr(self, key, value)
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        models.storage.new(self)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         my_className = self.__class__.__name__
@@ -32,7 +35,7 @@ class BaseModel:
 
     def to_dict(self):
         rslt = self.__dict__.copy()
-        rslt['__class__'] = self.__class__.__name__
         rslt['created_at'] = self.created_at.isoformat()
         rslt['updated_at'] = self.updated_at.isoformat()
+        rslt['__class__'] = self.__class__.__name__
         return rslt
